18c659ba1bba018c985c47eab8c61bd28de165ba
fatam0rgana <pazon4ik@gmail.com>
2022-09-13 15:18:42 -0700

    runtime update

--- todo_list/settings.py
+++ todo_list/settings.py
@@ -122,9 +122,9 @@ USE_TZ = True
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATIC_URL = 'static/'
 
-STATICFILES_DIRS = [
+'''STATICFILES_DIRS = [
     BASE_DIR / 'static'
-]
+]'''
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
