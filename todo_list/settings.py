7679fbc87ae9d1b8ad250e45b48aa9676c73938d
fatam0rgana <pazon4ik@gmail.com>
2022-09-13 16:29:36 -0700

    settings update

--- todo_list/settings.py
+++ todo_list/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 from datetime import timedelta
 from pathlib import Path
 import os
+import django_on_heroku
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -171,3 +172,4 @@ SIMPLE_JWT = {
     'SLIDING_TOKEN_LIFETIME': timedelta(minutes=5),
     'SLIDING_TOKEN_REFRESH_LIFETIME': timedelta(days=1),
 }
+django_on_heroku.settings(locals())
